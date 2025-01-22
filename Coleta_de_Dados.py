62f2266320417077a8d93b282491ab551a3cfd2d
RhuanMateusCMB <rhuanmateuscmb@gmail.com>
2025-01-22 13:20:46 -0300

    Update Coleta_de_Dados.py

--- Coleta_de_Dados.py
+++ Coleta_de_Dados.py
@@ -372,7 +372,7 @@ class ScraperVivaReal:
                 continue
         return None
 
-    def coletar_dados(self, num_paginas: int = 32) -> Optional[pd.DataFrame]:
+    def coletar_dados(self, num_paginas: int = 1) -> Optional[pd.DataFrame]:
         navegador = None
         todos_dados: List[Dict] = []
         id_global = 0
